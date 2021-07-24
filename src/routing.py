512149198b2015dcdf0c0ae916a52f8b1e5992fe
QianW <qian@hhu.edu.cn>
2021-07-24 19:16:18 +0800

    Add muskingum to routing.py

--- src/routing.py
+++ src/routing.py
@@ -4,16 +4,16 @@ import numpy as np
 
 def routing_parms(f_parms):
     routing_parms = json.load(open(f_parms, 'rb'))
-    CS = routing_parms['CS']
-    CI = routing_parms['CI']
-    CG = routing_parms['CG']
+    CS = routing_parms['CS'] # 地面径流消退系数
+    CI = routing_parms['CI'] # 壤中流消退系数
+    CG = routing_parms['CG'] # 地下径流消退系数
     L = np.floor(routing_parms['L'])
     n = np.floor(routing_parms['x1'])
-    K = routing_parms['K']
-    xx = routing_parms['xx']
+    K = routing_parms['K'] # 槽蓄系数
+    xx = routing_parms['xx'] # 流量比重系数
     periods = routing_parms['periods']
     dt = K
-    AA = routing_parms['AA']
+    AA = routing_parms['AA'] # 流域面积
 
     C0 = (0.5 * dt - K * xx)/(0.5 * dt + K - K * xx)
     C1 = (0.5 * dt + K * xx)/(0.5 * dt + K - K * xx)
@@ -39,8 +39,33 @@ def network_runoff(NT, L, QQ, QT, CS):
     return QQ
 
 
-def muskingum():
-    pass
+def muskingum(Q_1, QQ, n, C0, C1, C2):
+    """
+    Parameters:
+    ===========
+    Q_1: the first observed Q
+    QQ: simulated Q
+
+    Return:
+    =======
+    QC: 
+    """
+    QC1 = np.zeros((QQ.shape[0], n+1))
+    QC1 = Q_1; QC1[:, 0] = QQ # TODO: to check with Zhenya
+    if n > 0:
+        QC2 = np.zeros(QQ.shape[0])
+        QC3 = np.zeros(QQ.shape[0])
+        QC3 = Q_1
+        QC2 = QQ
+        for i in range(n): # TODO: check the value of n whether starting from 0 or 1
+            for t in range(QQ.shape[0] - 1):
+                QC3[t+1]= C0 * QC2[t+1] + C1 * QC2[t] + C2 * QC3[t]
+            QC2 = QC3
+        QC = QC2        
+    else:
+        QC = QQ
+    return QC
+# End muskingum()
 
 def routing(Q_obs, NT, CS, CI, CG, L, n, K, xx, dt, C0, C1, C2, periods, RI, RG, RS, AA):
     QS, QI, QG, QQ, QT = np.zeros(NT), np.zeros(NT), \
@@ -54,6 +79,10 @@ def routing(Q_obs, NT, CS, CI, CG, L, n, K, xx, dt, C0, C1, C2, periods, RI, RG,
 
     QQ = network_runoff(NT, L, QQ, QT, CS)
 
-    # Muskingum
+    QC = muskingum(Q_obs[0], QQ, n, C0, C1, C2)
+    
+    return QC
+
+# End routing()
 
 
